79e48799533bd4e6262fc24490babf362316e175
YeQun888 <1294642921@qq.com>
2018-12-28 17:14:36 +0800

    首次构建

---
+++ src/pages/home/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout">
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+    </Layout>
+  </div>
+</template>
+<script>
+export default {
+  name: 'home',
+}
+</script>
+<style lang="scss" scoped>
+.layout {
+  .ivu-layout-header {
+  }
+  .ivu-layout-content {
+  }
+  .ivu-layout-footer {
+  }
+}
+</style>
---
+++ src/service/getData.js
@@ -0,0 +1,58 @@
+import fetch from '../config/fetch'
+
+//手机号登录
+export const getLogin = (phone, password) => fetch('/login/cellphone', {
+  phone,
+  password,
+}, 'get');
+
+//推荐歌单
+export const getPersonalized = () => fetch('/personalized', {});
+
+//最新音乐
+export const getNewsong = () => fetch('/personalized/newsong', {});
+
+//热歌榜
+export const getHotList = (idx) => fetch('/top/list', {
+  idx,
+});
+
+//搜索
+export const getSearch = (keywords) => fetch('/search', {
+  keywords,
+});
+
+//热搜
+export const getHotSearch = () => fetch('/search/hot', {});
+
+//搜索建议
+export const getSuggest = (keywords) => fetch('/search/suggest', {
+  keywords,
+});
+
+//搜索多重匹配
+export const getMultimatch = (keywords) => fetch('/search/multimatch', {
+  keywords,
+});
+
+//获取歌单详情
+export const getDetail = (id) => fetch('/playlist/detail', {
+  id,
+});
+
+//获取歌曲url
+export const getSongUrl = (id) => fetch('/song/url', {
+  id,
+});
+
+
+//获取歌词
+export const getSongLyric = (id) => fetch('/lyric', {
+  id,
+});
+
+
+//获取单个歌曲详情
+export const getSongDetail = (ids) => fetch('/song/detail', {
+  ids,
+});
